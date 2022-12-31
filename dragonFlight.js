ee9b3775de3f5730b7f8679abe5d71a13b359649
swarpala <85331518+swarpala@users.noreply.github.com>
2022-12-31 13:22:44 +0000

    test

--- dragonFlight.js
+++ dragonFlight.js
@@ -1,5 +1,5 @@
 /** @type {CanvasRenderingContext2D} */
 
 const gameCanvas = document.body.getElementById('gameCanvas');
-const ctx = gameCanvas.getContext('2d');
+const gameCtx = gameCanvas.getContext('2d');
 
