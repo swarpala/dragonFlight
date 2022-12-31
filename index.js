e7f2103f2fb4e40c896fae549bd115a022ff2a1e
swarpala <85331518+swarpala@users.noreply.github.com>
2022-12-31 13:29:15 +0000

    commit message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /** @type {CanvasRenderingContext2D} */
 
-const gameCanvas = document.body.getElementById('gameCanvas');
+const gameCanvas = document.body.querySelector('#gameCanvas');
 const gameCtx = gameCanvas.getContext('2d');
 
